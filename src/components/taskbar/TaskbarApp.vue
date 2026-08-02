fda9dcafa5e867b091c8871cc67451e43a67e52d
RobertsTr <troksaroberts@gmail.com>
2026-08-02 16:52:28 +0300

    Bug fix

--- src/components/taskbar/TaskbarApp.vue
+++ src/components/taskbar/TaskbarApp.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+// import { ref } from 'vue'
 interface TaskbarApp{
     name: string
     src: string
@@ -9,7 +9,7 @@ const props = defineProps<{
   taskbarApp: TaskbarApp 
 }>()
 
-const isHovered = ref(false)
+// const isHovered = ref(false)
 
 </script>
 
