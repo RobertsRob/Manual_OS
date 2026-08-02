8dd676d74c2d6fb44034e8313c46734e37196a43
RobertsTr <troksaroberts@gmail.com>
2026-08-02 14:54:43 +0300

    Fixed errors, added github workflow

--- src/components/desktop/Icons.vue
+++ src/components/desktop/Icons.vue
@@ -6,13 +6,13 @@ const icons = [
     name: "Trash bin",
     src: "https://cdn-icons-png.flaticon.com/512/4021/4021663.png",
     shortcut: "C/abc/abcd",
-    position: [0, 0]
+    position: [0, 0] as [number, number]
     },
     {
     name: "Google Chrome",
     src: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Google_Chrome_icon_%28February_2022%29.svg/3840px-Google_Chrome_icon_%28February_2022%29.svg.png",
     shortcut: "C/abc/abc",
-    position: [0, 9.5]
+    position: [0, 9.5] as [number, number]
     }
 ]
 </script>
--- src/components/desktop/Wallpaper.vue
+++ src/components/desktop/Wallpaper.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-    import { ref } from 'vue'
-    import wallpaper1 from "../../assets/wallpaper1.jpg"
-    import wallpaper2 from "../../assets/wallpaper2.jpg"
+    // import { ref } from 'vue'
+    // import wallpaper1 from "../../assets/wallpaper1.jpg"
+    // import wallpaper2 from "../../assets/wallpaper2.jpg"
     import wallpaper3 from "../../assets/wallpaper3.jpg"
 </script>
 
